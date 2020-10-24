1f508774b7f10bb5a72c944aaa5a88a94447132d
Тараскин Михаил <MITaraskin@domclick.ru>
2020-10-24 11:09:53 +0300

    added methods #serialize_value, #serialize_to_hash Lutaml::LutamlPath::DocumentWrapper

--- lib/lutaml/lutaml_path/document_wrapper.rb
+++ lib/lutaml/lutaml_path/document_wrapper.rb
@@ -16,6 +16,25 @@ module Lutaml
       def serialize_document(_path)
         raise ArgumentError, "implement #serialize_document!"
       end
+
+      def serialize_value(attr_value)
+        if attr_value.is_a?(Array)
+          return attr_value.map(&method(:serialize_to_hash))
+        end
+
+        attr_value
+      end
+
+      def serialize_to_hash(object)
+        object.instance_variables.each_with_object({}) do |var, res|
+          variable = object.instance_variable_get(var)
+          if variable.is_a?(Array)
+            res[var.to_s.gsub("@", '')] = variable.map { |n| serialize_to_hash(n) }
+          else
+            res[var.to_s.gsub("@", '')] = variable
+          end
+        end
+      end
     end
   end
 end
