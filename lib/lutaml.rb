00e221f3436ebb3cb5bb84dd364aecd74d88f964
Тараскин Михаил <MITaraskin@domclick.ru>
2020-10-26 17:41:34 +0300

    lutaml/lutaml-express#2 Lutaml::Parser wrapper for determining lutaml parser

--- lib/lutaml.rb
+++ lib/lutaml.rb
@@ -1,7 +1,6 @@
 require "lutaml/version"
+require "lutaml/parser"
 require "lutaml/lutaml_path/document_wrapper"
 
 module Lutaml
-  class Error < StandardError; end
-  # Your code goes here...
 end
---
+++ spec/lutaml/parser_spec.rb
@@ -0,0 +1,18 @@
+require 'spec_helper'
+
+RSpec.describe Lutaml::Parser do
+  describe '.parse' do
+    subject(:parse) { described_class.parse(input) }
+
+    context 'when exp file supplied' do
+      let(:input) { File.new(fixtures_path('test.exp')) }
+
+      it 'calls Lutaml::Express::Parsers::Exp' do
+        allow(Lutaml::Express::Parsers::Exp).to receive(:parse)
+        allow(Lutaml::Express::LutamlPath::DocumentWrapper).to receive(:new)
+        parse
+        expect(Lutaml::Express::Parsers::Exp).to have_received(:parse)
+      end
+    end
+  end
+end
