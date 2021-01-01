bed5ef9bf5ca0efc87fab99c2f0ee7a8b5eff769
Тараскин Михаил <MITaraskin@domclick.ru>
2021-01-01 14:54:15 +0300

    Support fro `sourcecode` attribute, document wrapper, fixes for array of simple object serialization, new specs

--- lutaml.gemspec
+++ lutaml.gemspec
@@ -27,7 +27,6 @@ Gem::Specification.new do |spec|
   spec.add_runtime_dependency "thor", "~> 1.0"
   spec.add_runtime_dependency "lutaml-uml", "~> 0.2.5"
   spec.add_runtime_dependency "lutaml-express", "~> 0.1.3"
-  spec.add_development_dependency "jmespath", "~> 1.4"
   spec.add_development_dependency "nokogiri", "~> 1.10"
 
   spec.add_development_dependency "bundler", "~> 2.0"
--- spec/lutaml/express/lutaml_path/document_wrapper_spec.rb
+++ spec/lutaml/express/lutaml_path/document_wrapper_spec.rb
@@ -14,6 +14,7 @@ RSpec.describe Lutaml::Express::LutamlPath::DocumentWrapper do
         Lutaml::Express::Parsers::Exp.parse(File.new(fixtures_path("test.exp")))
       end
       let(:schema) { "annotated_3d_model_data_quality_criteria_schema" }
+      let(:schema_remark) { File.read(fixtures_path("schema_remark.txt")) }
       let(:entities_names) do
         %w[
           a3m_data_quality_criteria_representation
@@ -24,10 +25,26 @@ RSpec.describe Lutaml::Express::LutamlPath::DocumentWrapper do
           a3m_summary_report_request_with_representative_value
         ]
       end
+      let(:formatted_repository) { File.read(fixtures_path("test_formatted.exp")) }
+      let(:formatted_entity_1) { File.read(fixtures_path("test_formatted_entities_1.exp")) }
+      let(:formatted_entity_2) { File.read(fixtures_path("test_formatted_entities_2.exp")) }
 
       it "serializes repository attributes" do
         expect(serialized_document.keys).to(eq(["schemas", schema]))
-        expect(serialized_document["schemas"].map { |n| n["id"] }).to(eq([schema]))
+        expect(serialized_document["schemas"]
+                .map { |n| n["id"] })
+          .to(eq([schema]))
+        expect(serialized_document["schemas"].first['remarks'])
+          .to(eq([schema_remark.strip]))
+      end
+
+      it "merges source code into all schemas and their entities" do
+        expect(serialized_document["schemas"].first['sourcecode'])
+          .to(eq(formatted_repository))
+        expect(serialized_document["schemas"].first['entities'].first['sourcecode'])
+          .to(eq(formatted_entity_1))
+        expect(serialized_document["schemas"].first['entities'][1]['sourcecode'])
+          .to(eq(formatted_entity_2))
       end
 
       it "correctly finds elements by jmespath expression" do
