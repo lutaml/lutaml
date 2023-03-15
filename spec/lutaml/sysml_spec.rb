865fcc690e13fc1a40f8ce9826bd69813e145d73
Ronald Tse <ronald.tse@ribose.com>
2023-03-15 09:58:27 +0800

    chore: no specs for now

--- spec/lutaml/sysml_spec.rb
+++ spec/lutaml/sysml_spec.rb
@@ -3,7 +3,7 @@ RSpec.describe Lutaml::Sysml do
     expect(Lutaml::Sysml::VERSION).not_to be nil
   end
 
-  it "does something useful" do
-    expect(false).to eq(true)
-  end
+  # it "does something useful" do
+  #   expect(false).to eq(true)
+  # end
 end
