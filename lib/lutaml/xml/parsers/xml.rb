686e086abfb044bf860b9918ee6f20f30619439b
HassanAkbar <hassanakbar2@gmail.com>
2024-04-23 17:20:01 +0500

    updated to use schema for reading xml data

--- lib/lutaml/xml/parsers/xml.rb
+++ lib/lutaml/xml/parsers/xml.rb
@@ -3,31 +3,53 @@ require "htmlentities"
 require "lutaml/uml/has_attributes"
 require "lutaml/uml/document"
 
+require "shale/schema"
+
 module Lutaml
   module Xml
     module Parsers
       # Class for parsing .xml schema files into ::Lutaml::Uml::Document
       class Xml
-        def self.set_document(doc)
-          @@document = doc
+        def self.load_schema(schema, root_schema)
+          result = Shale::Schema.from_xml([schema])
+
+          result.values.each do |klass|
+            # Temporary solution will update in the parser
+            klass = klass.gsub(/^require.*?\n/, "")
+            klass = klass.gsub(/< Shale::Mapper/, "< Lutaml::Xml::Mapper")
+
+            eval(klass, TOPLEVEL_BINDING)
+          end
+
+          @@root_schema = root_schema
         end
 
         def self.parse(file)
-          new(file).parse
+          new(file, @@root_schema).parse
         end
 
-        def initialize(file)
+        def initialize(file, root_schema)
           @file = file
+          @root_schema = root_schema
+          @root_class = Object.const_get(root_schema)
         end
 
         def parse
-          unless defined?(@@document)
-            msg = "document is not defined for XML. Set it by Lutaml::Xml::Parsers::Xml.set_document(base_document) method"
-            raise StandardError, msg
-          end
-
           doc = File.read(@file)
-          @@document.from_xml(doc)
+
+          @root_class.from_xml(doc)
+        end
+
+        private
+
+        def load_schema(schema)
+          result = Shale::Schema.from_xml([schema])
+
+          result.values.each do |klass|
+            klass = klass.gsub(/^require.*?\n/, "")
+
+            eval(klass, TOPLEVEL_BINDING)
+          end
         end
       end
     end
