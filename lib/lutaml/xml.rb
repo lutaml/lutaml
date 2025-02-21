5e53b7114d6b2e5bf748df1d8fe53fa0309b26c5
KW Kwan <kw.kwan@ribose.com>
2025-02-21 12:21:48 +0800

    Remove DocumentWrapper

--- lib/lutaml/xml.rb
+++ lib/lutaml/xml.rb
@@ -1,4 +1,1 @@
 require "lutaml/xml/parsers/xml"
-
-# require "lutaml/xml/mapper"
-require "lutaml/xml/lutaml_path/document_wrapper"
--- spec/lutaml/xml/parsers/xml_spec.rb
+++ spec/lutaml/xml/parsers/xml_spec.rb
@@ -13,6 +13,25 @@ RSpec.describe Lutaml::Xml::Parsers::Xml do
       File.read(fixtures_path("schema.xml"))
     end
 
+    let(:expected_hash) do
+      {
+        "age" => 50,
+        "first_name" => ["John"],
+        "last_name" => ["Doe"],
+        "hobby" => ["Singing", "Dancing"],
+        "address" => [
+          {
+            "city" => ["London"],
+            "zip" => ["E1 6AN"],
+            "content" => [
+              "\n    Oxford Street\n    ",
+              "\n    ", "\n  "
+            ],
+          },
+        ],
+      }
+    end
+
     context "correctly parses xml document" do
       let(:xml_file_path) { fixtures_path("test.xml") }
       let(:root_name) { "Person" }
@@ -22,10 +41,15 @@ RSpec.describe Lutaml::Xml::Parsers::Xml do
         expect(parsed).to be_instance_of(Person)
       end
 
-      it "expect to read the xml file" do
+      it "outputs xml as same as the xml file it reads" do
         input_xml = File.read(xml_file_path)
         formatted_xml = Nokogiri::XML(parse.to_xml).root.to_xml
         expect(formatted_xml).to be_equivalent_to(input_xml)
+      end
+
+      it "parses xml file and able to output hash" do
+        parsed = parse
+        expect(JSON.parse(parsed.to_json)).to eq(expected_hash)
       end
     end
   end
