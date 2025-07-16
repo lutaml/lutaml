632694099d18b9d5222e8782622af29b87be540b
suleman-uzair <suleman@workytical.com>
2025-07-16 13:55:25 +0500

    supported processing XSD schemas using lutaml-xsd

--- lib/lutaml/parser.rb
+++ lib/lutaml/parser.rb
@@ -11,16 +11,17 @@ module Lutaml
     attr_reader :parse_type, :file_list
 
     class << self
-      def parse(file_list, input_type = nil)
+      def parse(file_list, input_type = nil, options: {})
         file_list = [file_list] unless file_list.is_a?(Array)
-        new(Array(file_list), input_type).parse_into_document
+        new(Array(file_list), input_type, options).parse_into_document
       end
       alias_method :parse_into_document, :parse
     end
 
-    def initialize(file_list, input_type)
+    def initialize(file_list, input_type, options = {})
       @parse_type = input_type || File.extname(file_list.first.path)[1..-1]
       @file_list = file_list
+      @options = options
     end
 
     def parse_into_document # rubocop:disable Metrics/AbcSize,Metrics/CyclomaticComplexity,Metrics/MethodLength,Metrics/PerceivedComplexity
@@ -37,6 +38,11 @@ module Lutaml
         file_list.map { |file| Lutaml::Uml::Parsers::Dsl.parse(file) }
       when "yml", "yaml"
         file_list.map { |file| Lutaml::Uml::Parsers::Yaml.parse(file.path) }
+      when "xsd"
+        Lutaml::Xsd.parse(
+          file_list.first.read,
+          location: @options["location"],
+        )
       else
         raise ArgumentError, "Unsupported file format"
       end
