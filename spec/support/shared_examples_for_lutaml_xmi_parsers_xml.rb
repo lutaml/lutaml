b686b9bd8efcac8a09c3422303c35951b62d1501
KW Kwan <kw.kwan@ribose.com>
2025-12-03 10:22:12 +0800

    Modify the namespaces of the liquid drop classes to match the recent changes

--- spec/support/shared_examples_for_lutaml_xmi_parsers_xml.rb
+++ spec/support/shared_examples_for_lutaml_xmi_parsers_xml.rb
@@ -1,6 +1,6 @@
 RSpec.shared_examples "should output correct klass liquid drop" do |id, name|
-  it "outputs Lutaml::Xmi::KlassDrop" do
-    expect(output).to be_instance_of(Lutaml::Xmi::KlassDrop)
+  it "outputs Lutaml::Xmi::LiquidDrops::KlassDrop" do
+    expect(output).to be_instance_of(Lutaml::Xmi::LiquidDrops::KlassDrop)
   end
 
   it "correctly parses model name" do
@@ -13,7 +13,7 @@ RSpec.shared_examples "should output correct klass liquid drop" do |id, name|
 
   it "correctly outputs generalization" do
     expect(output.generalization).to(
-      be_instance_of(Lutaml::Xmi::GeneralizationDrop),
+      be_instance_of(Lutaml::Xmi::LiquidDrops::GeneralizationDrop),
     )
   end
 
