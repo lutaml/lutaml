6b865a5214a53a0d8145958e4d7aa84112229e64
Ronald Tse <ronald.tse@ribose.com>
2026-05-12 22:41:31 +0800

    refactor(qea): extract helper methods from god methods in factory classes
    Break down oversized methods in QEA factory modules to reduce
    RuboCop Metrics offenses (AbcSize, MethodLength, CyclomaticComplexity).
    
    - association_builder: extract build_association, resolve_member_object,
      assign_association_ends, build_end_attribute, build_start_attribute
    - class_transformer: extract transformable?, assign_basic_properties,
      assign_features, assign_relationships from monolithic transform
    - generalization_builder: extract build_generalization, ea_connector_for,
      populate_generalization_attrs, base_attr_hash
    - enum_transformer: extract enum?, valid_stereotype?, build_enum_value
    - stereotype_loader: extract find_stereotype_xref, extract_stereotype_name
    - lookup_indexes: split build_lookup_indexes into build_primary_indexes +
      build_secondary_indexes

--- lib/lutaml/qea/factory/association_builder.rb
+++ lib/lutaml/qea/factory/association_builder.rb
@@ -11,147 +11,42 @@ module Lutaml
         def load_class_associations(object_id, object_guid)
           return [] if object_id.nil?
 
-          associations = []
           normalized_owner_xmi_id = normalize_guid_to_xmi_format(object_guid,
                                                                  "EAID")
 
           assoc_connectors = database.connectors_for_object(object_id)
             .select { |c| ASSOC_TYPES.include?(c.connector_type) }
 
-          assoc_connectors.each do |ea_connector|
-            is_start = ea_connector.start_object_id == object_id
-
-            owner_end_attribute_name = if is_start
-                                         ea_connector.destrole
-                                       else
-                                         ea_connector.sourcerole
-                                       end
-
-            if owner_end_attribute_name.nil? || owner_end_attribute_name.empty?
-              next
-            end
-
-            member_obj = if is_start
-                           find_object_by_id(ea_connector.end_object_id)
-                         else
-                           find_object_by_id(ea_connector.start_object_id)
-                         end
-            next unless member_obj
-
-            member_end_attribute_name = if is_start
-                                          ea_connector.sourcerole
-                                        else
-                                          ea_connector.destrole
-                                        end
-            if member_end_attribute_name.nil? ||
-                member_end_attribute_name.empty?
-              member_end_attribute_name = member_obj.name
-            end
-
-            member_cardinality_str = if is_start
-                                       ea_connector.destcard
-                                     else
-                                       ea_connector.sourcecard
-                                     end
-
-            associations << Lutaml::Uml::Association.new.tap do |assoc|
-              assoc.xmi_id = normalize_guid_to_xmi_format(ea_connector.ea_guid,
-                                                          "EAID")
-              assoc.name = ea_connector.name unless ea_connector.name.nil? || ea_connector.name.empty?
-
-              assoc.owner_end = find_object_by_id(object_id)&.name
-              assoc.owner_end_xmi_id = normalized_owner_xmi_id
-              assoc.owner_end_attribute_name = owner_end_attribute_name
-
-              assoc.member_end = member_obj.name
-              assoc.member_end_xmi_id = normalize_guid_to_xmi_format(
-                member_obj.ea_guid, "EAID"
-              )
-              assoc.member_end_attribute_name = member_end_attribute_name
-
-              assoc.member_end_type = ea_connector.connector_type&.downcase
-
-              if member_cardinality_str && !member_cardinality_str.empty?
-                parsed = parse_cardinality(member_cardinality_str)
-                if parsed[:min] || parsed[:max]
-                  assoc.member_end_cardinality = Lutaml::Uml::Cardinality.new
-                    .tap do |card|
-                      card.min = parsed[:min]
-                      card.max = parsed[:max]
-                    end
-                end
-              end
-            end
+          assoc_connectors.filter_map do |ea_connector|
+            build_association(ea_connector, object_id, normalized_owner_xmi_id)
           end
-
-          associations.compact
         end
 
         def load_association_attributes(object_id)
           return [] if object_id.nil?
 
-          attributes = []
           assoc_connectors = database.connectors_for_object(object_id)
             .select { |c| ASSOC_TYPES.include?(c.connector_type) }
           obj = find_object_by_id(object_id)
           obj_pkg_name = find_package_name(obj&.package_id)
 
-          assoc_connectors.each do |ea_connector|
+          assoc_connectors.filter_map do |ea_connector|
             if ea_connector.start_object_id == object_id
-              next if ea_connector.destrole.nil? || ea_connector.destrole.empty?
-
-              target_obj = find_object_by_id(ea_connector.end_object_id)
-              next unless target_obj
-
-              target_obj_pkg_name = find_package_name(target_obj.package_id)
-
-              attributes << create_association_attribute(
-                name: ea_connector.destrole,
-                type: target_obj.name,
-                type_xmi_id: target_obj.ea_guid,
-                association_xmi_id: ea_connector.ea_guid,
-                cardinality: ea_connector.destcard,
-                definition: ea_connector.notes,
-                gen_name: obj.name,
-                name_ns: obj_pkg_name,
-                type_ns: target_obj_pkg_name,
-                is_src: false,
-              )
+              build_end_attribute(ea_connector, obj, obj_pkg_name)
             elsif ea_connector.end_object_id == object_id
-              next if ea_connector.sourcerole.nil? || ea_connector.sourcerole.empty?
-
-              source_obj = find_object_by_id(ea_connector.start_object_id)
-              next unless source_obj
-
-              source_obj_pkg_name = find_package_name(source_obj.package_id)
-
-              attributes << create_association_attribute(
-                name: ea_connector.sourcerole,
-                type: source_obj.name,
-                type_xmi_id: source_obj.ea_guid,
-                association_xmi_id: ea_connector.ea_guid,
-                cardinality: ea_connector.sourcecard,
-                definition: ea_connector.notes,
-                gen_name: obj.name,
-                name_ns: obj_pkg_name,
-                type_ns: source_obj_pkg_name,
-              )
+              build_start_attribute(ea_connector, obj, obj_pkg_name)
             end
           end
-
-          attributes.compact
         end
 
-        def create_association_attribute( # rubocop:disable Metrics/AbcSize,Metrics/MethodLength,Metrics/ParameterLists
+        def create_association_attribute( # rubocop:disable Metrics/ParameterLists
           name:, type:, type_xmi_id:,
           association_xmi_id:, cardinality:, definition:,
           gen_name:, name_ns:, type_ns:, is_src: true
         )
           Lutaml::Uml::GeneralAttribute.new.tap do |attr|
-            attr.name = name
-            attr.type = type
-            attr.gen_name = gen_name
-            attr.definition = definition
+            assign_assoc_attr_basic(attr, name, type, gen_name, definition,
+                                    name_ns, type_ns)
             attr.xmi_id = normalize_guid_to_xmi_format(type_xmi_id, "EAID")
             attr.association = normalize_guid_to_xmi_format(
               association_xmi_id, "EAID"
@@ -160,22 +55,116 @@ module Lutaml
             attr.id = normalize_guid_to_xmi_src_dst_format(
               association_xmi_id, "EAID", is_src
             )
-            attr.name_ns = name_ns
-            attr.type_ns = type_ns
-
-            if cardinality && !cardinality.empty?
-              parsed = parse_cardinality(cardinality)
-              if parsed[:min] || parsed[:max]
-                attr.cardinality = Lutaml::Uml::Cardinality.new.tap do |card|
-                  card.min = parsed[:min]
-                  card.max = parsed[:max]
-                end
-              end
-            end
+            attr.cardinality = build_cardinality(cardinality)
           end
         end
 
         private
+
+        def assign_assoc_attr_basic(attr, name, type, gen_name,
+                                    definition, name_ns, type_ns)
+          attr.name = name
+          attr.type = type
+          attr.gen_name = gen_name
+          attr.definition = definition
+          attr.name_ns = name_ns
+          attr.type_ns = type_ns
+        end
+
+        def build_association(ea_connector, object_id, normalized_owner_xmi_id)
+          is_start = ea_connector.start_object_id == object_id
+          owner_role = is_start ? ea_connector.destrole : ea_connector.sourcerole
+          return nil if owner_role.nil? || owner_role.empty?
+
+          member_obj = resolve_member_object(ea_connector, is_start)
+          return nil unless member_obj
+
+          member_role = resolve_member_role(ea_connector, is_start, member_obj)
+          cardinality_str = is_start ? ea_connector.destcard : ea_connector.sourcecard
+
+          Lutaml::Uml::Association.new.tap do |assoc|
+            assoc.xmi_id = normalize_guid_to_xmi_format(ea_connector.ea_guid,
+                                                        "EAID")
+            assoc.name = ea_connector.name unless ea_connector.name.nil? || ea_connector.name.empty?
+            assign_association_ends(assoc, object_id, normalized_owner_xmi_id,
+                                    owner_role, member_obj, member_role)
+            assoc.member_end_type = ea_connector.connector_type&.downcase
+            assoc.member_end_cardinality = build_cardinality(cardinality_str)
+          end
+        end
+
+        def assign_association_ends(assoc, object_id, owner_xmi_id,
+                                    owner_role, member_obj, member_role)
+          assoc.owner_end = find_object_by_id(object_id)&.name
+          assoc.owner_end_xmi_id = owner_xmi_id
+          assoc.owner_end_attribute_name = owner_role
+          assoc.member_end = member_obj.name
+          assoc.member_end_xmi_id = normalize_guid_to_xmi_format(
+            member_obj.ea_guid, "EAID"
+          )
+          assoc.member_end_attribute_name = member_role
+        end
+
+        def resolve_member_object(ea_connector, is_start)
+          peer_id = is_start ? ea_connector.end_object_id : ea_connector.start_object_id
+          find_object_by_id(peer_id)
+        end
+
+        def resolve_member_role(ea_connector, is_start, member_obj)
+          role = is_start ? ea_connector.sourcerole : ea_connector.destrole
+          role.nil? || role.empty? ? member_obj.name : role
+        end
+
+        def build_end_attribute(ea_connector, obj, obj_pkg_name)
+          return nil if ea_connector.destrole.nil? || ea_connector.destrole.empty?
+
+          target_obj = find_object_by_id(ea_connector.end_object_id)
+          return nil unless target_obj
+
+          create_association_attribute(
+            name: ea_connector.destrole,
+            type: target_obj.name,
+            type_xmi_id: target_obj.ea_guid,
+            association_xmi_id: ea_connector.ea_guid,
+            cardinality: ea_connector.destcard,
+            definition: ea_connector.notes,
+            gen_name: obj.name,
+            name_ns: obj_pkg_name,
+            type_ns: find_package_name(target_obj.package_id),
+            is_src: false,
+          )
+        end
+
+        def build_start_attribute(ea_connector, obj, obj_pkg_name)
+          return nil if ea_connector.sourcerole.nil? || ea_connector.sourcerole.empty?
+
+          source_obj = find_object_by_id(ea_connector.start_object_id)
+          return nil unless source_obj
+
+          create_association_attribute(
+            name: ea_connector.sourcerole,
+            type: source_obj.name,
+            type_xmi_id: source_obj.ea_guid,
+            association_xmi_id: ea_connector.ea_guid,
+            cardinality: ea_connector.sourcecard,
+            definition: ea_connector.notes,
+            gen_name: obj.name,
+            name_ns: obj_pkg_name,
+            type_ns: find_package_name(source_obj.package_id),
+          )
+        end
+
+        def build_cardinality(cardinality_str)
+          return nil unless cardinality_str && !cardinality_str.empty?
+
+          parsed = parse_cardinality(cardinality_str)
+          return nil unless parsed[:min] || parsed[:max]
+
+          Lutaml::Uml::Cardinality.new.tap do |card|
+            card.min = parsed[:min]
+            card.max = parsed[:max]
+          end
+        end
 
         def find_package_name(package_id)
           return nil if package_id.nil?
