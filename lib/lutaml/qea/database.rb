d1ababa9d0963cf16adf1cf7b91674819d6bea66
Ronald Tse <ronald.tse@ribose.com>
2026-05-13 15:10:36 +0800

    refactor: DRY QEA database with declarative collection accessors
    Replace 20+ identical collection accessor methods and 12+ lazy-indexed
    finder methods with constant-driven DSL via define_method. Reduces
    database.rb from 431 to 207 lines.

--- lib/lutaml/qea/database.rb
+++ lib/lutaml/qea/database.rb
@@ -54,7 +54,22 @@ module Lutaml
         end
       end
 
-      # Get objects collection
+      COLLECTION_ACCESSORS = %i[
+        attributes operations operation_params connectors packages
+        diagrams diagram_objects diagram_links object_constraints
+        tagged_values object_properties attribute_tags xrefs
+        stereotypes datatypes constraint_types connector_types
+        diagram_types object_types status_types complexity_types
+        documents scripts
+      ].freeze
+
+      COLLECTION_ACCESSORS.each do |name|
+        define_method(name) do
+          @collections[name] || []
+        end
+      end
+
+      # Get objects collection (special: wrapped in ObjectRepository)
       #
       # @return [Repositories::ObjectRepository] Repository for objects
       def objects
@@ -63,172 +78,6 @@ module Lutaml
         @objects = Repositories::ObjectRepository.new(
           @collections[:objects] || [],
         )
-      end
-
-      # Get attributes collection
-      #
-      # @return [Array<Models::EaAttribute>] Array of attributes
-      def attributes
-        @collections[:attributes] || []
-      end
-
-      # Get operations collection
-      #
-      # @return [Array<Models::EaOperation>] Array of operations
-      def operations
-        @collections[:operations] || []
-      end
-
-      # Get operation parameters collection
-      #
-      # @return [Array<Models::EaOperationParam>] Array of operation parameters
-      def operation_params
-        @collections[:operation_params] || []
-      end
-
-      # Get connectors collection
-      #
-      # @return [Array<Models::EaConnector>] Array of connectors
-      def connectors
-        @collections[:connectors] || []
-      end
-
-      # Get packages collection
-      #
-      # @return [Array<Models::EaPackage>] Array of packages
-      def packages
-        @collections[:packages] || []
-      end
-
-      # Get diagrams collection
-      #
-      # @return [Array<Models::EaDiagram>] Array of diagrams
-      def diagrams
-        @collections[:diagrams] || []
-      end
-
-      # Get diagram objects collection (visual placement)
-      #
-      # @return [Array<Models::EaDiagramObject>] Array of diagram objects
-      def diagram_objects
-        @collections[:diagram_objects] || []
-      end
-
-      # Get diagram links collection (visual routing)
-      #
-      # @return [Array<Models::EaDiagramLink>] Array of diagram links
-      def diagram_links
-        @collections[:diagram_links] || []
-      end
-
-      # Get object constraints collection
-      #
-      # @return [Array<Models::EaObjectConstraint>] Array of object
-      #   constraints
-      def object_constraints
-        @collections[:object_constraints] || []
-      end
-
-      # Get tagged values collection
-      #
-      # @return [Array<Models::EaTaggedValue>] Array of tagged values
-      def tagged_values
-        @collections[:tagged_values] || []
-      end
-
-      # Get object properties collection
-      #
-      # @return [Array<Models::EaObjectProperty>] Array of object properties
-      def object_properties
-        @collections[:object_properties] || []
-      end
-
-      # Get attribute tags collection
-      #
-      # @return [Array<Models::EaAttributeTag>] Array of attribute tags
-      def attribute_tags
-        @collections[:attribute_tags] || []
-      end
-
-      # Get cross-references collection
-      #
-      # @return [Array<Models::EaXref>] Array of cross-references
-      def xrefs
-        @collections[:xrefs] || []
-      end
-
-      # Get stereotypes collection
-      #
-      # @return [Array<Models::EaStereotype>] Array of stereotype definitions
-      def stereotypes
-        @collections[:stereotypes] || []
-      end
-
-      # Get datatypes collection
-      #
-      # @return [Array<Models::EaDatatype>] Array of datatype definitions
-      def datatypes
-        @collections[:datatypes] || []
-      end
-
-      # Get constraint types collection (Priority 3 lookup table)
-      #
-      # @return [Array<Models::EaConstraintType>] Array of constraint type
-      #   definitions
-      def constraint_types
-        @collections[:constraint_types] || []
-      end
-
-      # Get connector types collection (Priority 3 lookup table)
-      #
-      # @return [Array<Models::EaConnectorType>] Array of connector type
-      #   definitions
-      def connector_types
-        @collections[:connector_types] || []
-      end
-
-      # Get diagram types collection (Priority 3 lookup table)
-      #
-      # @return [Array<Models::EaDiagramType>] Array of diagram type
-      #   definitions
-      def diagram_types
-        @collections[:diagram_types] || []
-      end
-
-      # Get object types collection (Priority 3 lookup table)
-      #
-      # @return [Array<Models::EaObjectType>] Array of object type definitions
-      def object_types
-        @collections[:object_types] || []
-      end
-
-      # Get status types collection (Priority 3 lookup table)
-      #
-      # @return [Array<Models::EaStatusType>] Array of status type definitions
-      def status_types
-        @collections[:status_types] || []
-      end
-
-      # Get complexity types collection (Priority 3 lookup table)
-      #
-      # @return [Array<Models::EaComplexityType>] Array of complexity type
-      #   definitions
-      def complexity_types
-        @collections[:complexity_types] || []
-      end
-
-      # Get documents collection (Priority 4)
-      #
-      # @return [Array<Models::EaDocument>] Array of document artifacts
-      def documents
-        @collections[:documents] || []
-      end
-
-      # Get scripts collection (Priority 4)
-      #
-      # @return [Array<Models::EaScript>] Array of behavioral scripts
-      def scripts
-        @collections[:scripts] || []
       end
 
       # Get statistics for all collections
@@ -256,6 +105,48 @@ module Lutaml
         @collections.values.sum(&:size)
       end
 
+      SINGLE_INDEXES = {
+        find_package: [:packages, :package_id],
+        find_attribute: [:attributes, :id],
+        find_connector: [:connectors, :connector_id],
+        find_diagram: [:diagrams, :diagram_id],
+      }.freeze
+
+      GROUP_INDEXES = {
+        attributes_for_object: [:attributes, :ea_object_id],
+        operations_for_object: [:operations, :ea_object_id],
+        operation_params_for: [:operation_params, :operationid],
+        child_packages_for: [:packages, :parent_id],
+        objects_in_package: [:objects, :package_id],
+        diagrams_in_package: [:diagrams, :package_id],
+        diagram_objects_for: [:diagram_objects, :diagram_id],
+        diagram_links_for: [:diagram_links, :diagramid],
+      }.freeze
+
+      SINGLE_INDEXES.each do |method_name, (collection, field)|
+        ivar = :"@#{method_name}_idx"
+        define_method(method_name) do |id|
+          idx = instance_variable_get(ivar)
+          unless idx
+            idx = build_group_index(public_send(collection), field, single: true)
+            instance_variable_set(ivar, idx)
+          end
+          idx[id]
+        end
+      end
+
+      GROUP_INDEXES.each do |method_name, (collection, field)|
+        ivar = :"@#{method_name}_idx"
+        define_method(method_name) do |id|
+          idx = instance_variable_get(ivar)
+          unless idx
+            idx = build_group_index(public_send(collection), field)
+            instance_variable_set(ivar, idx)
+          end
+          idx[id] || []
+        end
+      end
+
       # Find an object by ID
       #
       # @param id [Integer] Object ID
@@ -264,43 +155,24 @@ module Lutaml
         objects.find_by_key(:ea_object_id, id)
       end
 
-      # Find a package by ID
+      # Find object by ea_guid
       #
-      # @param id [Integer] Package ID
-      # @return [Models::EaPackage, nil] The package or nil if not found
-      def find_package(id)
-        @packages_by_id ||= build_group_index(packages, :package_id,
-                                              single: true)
-        @packages_by_id[id]
+      # @param ea_guid [String] Object GUID
+      # @return [Models::EaObject, nil] The object or nil if not found
+      def find_object_by_guid(ea_guid)
+        @objects_by_guid ||= build_group_index(objects, :ea_guid, single: true)
+        @objects_by_guid[ea_guid]
       end
 
-      # Find an attribute by ID
+      # Get connectors involving a specific object (start or end)
       #
-      # @param id [Integer] Attribute ID
-      # @return [Models::EaAttribute, nil] The attribute or nil if not found
-      def find_attribute(id)
-        @attributes_by_id ||= build_group_index(attributes, :id, single: true)
-        @attributes_by_id[id]
-      end
-
-      # Find a connector by ID
-      #
-      # @param id [Integer] Connector ID
-      # @return [Models::EaConnector, nil] The connector or nil if not found
-      def find_connector(id)
-        @connectors_by_id ||= build_group_index(connectors, :connector_id,
-                                                single: true)
-        @connectors_by_id[id]
-      end
-
-      # Find a diagram by ID
-      #
-      # @param id [Integer] Diagram ID
-      # @return [Models::EaDiagram, nil] The diagram or nil if not found
-      def find_diagram(id)
-        @diagrams_by_id ||= build_group_index(diagrams, :diagram_id,
-                                              single: true)
-        @diagrams_by_id[id]
+      # @param object_id [Integer] Object ID
+      # @return [Array<Models::EaConnector>] Connectors for the object
+      def connectors_for_object(object_id)
+        @connectors_by_start ||= build_group_index(connectors, :start_object_id)
+        @connectors_by_end ||= build_group_index(connectors, :end_object_id)
+        (@connectors_by_start[object_id] || []) +
+          (@connectors_by_end[object_id] || [])
       end
 
       # Check if database is empty
@@ -315,102 +187,6 @@ module Lutaml
       # @return [Array<Symbol>] Array of collection names
       def collection_names
         @collections.keys
-      end
-
-      # Find object by ea_guid
-      #
-      # @param ea_guid [String] Object GUID
-      # @return [Models::EaObject, nil] The object or nil if not found
-      def find_object_by_guid(ea_guid)
-        @objects_by_guid ||= build_group_index(objects, :ea_guid, single: true)
-        @objects_by_guid[ea_guid]
-      end
-
-      # Get attributes for a specific object
-      #
-      # @param object_id [Integer] Object ID
-      # @return [Array<Models::EaAttribute>] Attributes for the object
-      def attributes_for_object(object_id)
-        @attributes_by_object_id ||= build_group_index(attributes,
-                                                       :ea_object_id)
-        @attributes_by_object_id[object_id] || []
-      end
-
-      # Get operations for a specific object
-      #
-      # @param object_id [Integer] Object ID
-      # @return [Array<Models::EaOperation>] Operations for the object
-      def operations_for_object(object_id)
-        @operations_by_object_id ||= build_group_index(operations,
-                                                       :ea_object_id)
-        @operations_by_object_id[object_id] || []
-      end
-
-      # Get operation parameters for a specific operation
-      #
-      # @param operation_id [Integer] Operation ID
-      # @return [Array<Models::EaOperationParam>] Parameters for the operation
-      def operation_params_for(operation_id)
-        @operation_params_by_id ||= build_group_index(operation_params,
-                                                      :operationid)
-        @operation_params_by_id[operation_id] || []
-      end
-
-      # Get connectors involving a specific object (start or end)
-      #
-      # @param object_id [Integer] Object ID
-      # @return [Array<Models::EaConnector>] Connectors for the object
-      def connectors_for_object(object_id)
-        @connectors_by_start ||= build_group_index(connectors, :start_object_id)
-        @connectors_by_end ||= build_group_index(connectors, :end_object_id)
-        (@connectors_by_start[object_id] || []) +
-          (@connectors_by_end[object_id] || [])
-      end
-
-      # Get child packages for a parent
-      #
-      # @param parent_id [Integer] Parent package ID
-      # @return [Array<Models::EaPackage>] Child packages
-      def child_packages_for(parent_id)
-        @packages_by_parent ||= build_group_index(packages, :parent_id)
-        @packages_by_parent[parent_id] || []
-      end
-
-      # Get objects in a specific package
-      #
-      # @param package_id [Integer] Package ID
-      # @return [Array<Models::EaObject>] Objects in the package
-      def objects_in_package(package_id)
-        @objects_by_package_id ||= build_group_index(objects, :package_id)
-        @objects_by_package_id[package_id] || []
-      end
-
-      # Get diagrams in a specific package
-      #
-      # @param package_id [Integer] Package ID
-      # @return [Array<Models::EaDiagram>] Diagrams in the package
-      def diagrams_in_package(package_id)
-        @diagrams_by_package_id ||= build_group_index(diagrams, :package_id)
-        @diagrams_by_package_id[package_id] || []
-      end
-
-      # Get diagram objects for a specific diagram
-      #
-      # @param diagram_id [Integer] Diagram ID
-      # @return [Array<Models::EaDiagramObject>] Diagram objects
-      def diagram_objects_for(diagram_id)
-        @diagram_objects_by_id ||= build_group_index(diagram_objects,
-                                                     :diagram_id)
-        @diagram_objects_by_id[diagram_id] || []
-      end
-
-      # Get diagram links for a specific diagram
-      #
-      # @param diagram_id [Integer] Diagram ID
-      # @return [Array<Models::EaDiagramLink>] Diagram links
-      def diagram_links_for(diagram_id)
-        @diagram_links_by_id ||= build_group_index(diagram_links, :diagramid)
-        @diagram_links_by_id[diagram_id] || []
       end
 
       # Freeze all collections to make database immutable
