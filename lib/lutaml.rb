546ae70bb40a5b00617ece056a693f02545261f9
Ronald Tse <ronald.tse@ribose.com>
2026-05-23 20:11:46 +0800

    feat: wire up lutaml-lml as dependency in metagem
    - Add lutaml-lml to gemspec dependencies and Gemfile
    - Replace lutaml/formatter and lutaml/layout requires with lutaml/lml
    - All specs passing, CLI working

--- lib/lutaml.rb
+++ lib/lutaml.rb
@@ -1,14 +1,13 @@
 # frozen_string_literal: true
 
 require_relative "lutaml/version"
+require "lutaml/lml"
 require "lutaml/uml"
 require "lutaml/uml_repository"
 require "lutaml/converter"
 require "lutaml/ea"
 require "lutaml/xmi"
 require "lutaml/qea"
-require "lutaml/formatter"
-require "lutaml/layout"
 require "lutaml/model_transformations"
 require "lutaml/cli"
 
