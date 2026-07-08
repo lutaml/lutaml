779a1b6e64a9e62ab11233f900462b193907ce59
Ronald Tse <ronald.tse@ribose.com>
2026-07-08 22:10:29 +0800

    fix: remove 6 broken requires + bump to 0.11.0
    Removed requires for files that don't exist in the meta-gem:
    - lutaml/converter
    - lutaml/ea (ea uses Ea:: namespace, not Lutaml::Ea)
    - lutaml/xmi (xmi uses Xmi:: namespace, not Lutaml::Xmi)
    - lutaml/qea
    - lutaml/model_transformations
    - lutaml/cli
    
    These phantom requires caused 'require "lutaml"' to crash with
    LoadError on every install. The meta-gem now only requires its
    actual dependencies (lutaml-lml, lutaml-uml, uml_repository).

--- lib/lutaml.rb
+++ lib/lutaml.rb
@@ -4,12 +4,6 @@ require_relative "lutaml/version"
 require "lutaml/lml"
 require "lutaml/uml"
 require "lutaml/uml_repository"
-require "lutaml/converter"
-require "lutaml/ea"
-require "lutaml/xmi"
-require "lutaml/qea"
-require "lutaml/model_transformations"
-require "lutaml/cli"
 
 module Lutaml
   class Error < StandardError; end
